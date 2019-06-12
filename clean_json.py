0b34c6620fc5dacc3c6d47ad966b754669b4da6c
Jessie Tan <tanjiexi@bu.edu>
2019-06-12 18:58:47 -0400

    add a clean_json.py

---
+++ clean_json.py
@@ -0,0 +1,38 @@
+import simplejson as json
+def clean(t):
+   output=""
+   i=0
+   seen=0
+   while(i<len(t)):
+       if(t[i]=='"'):
+           if(t[i:i+8]=='"url": "'):
+               output=output+ '"url": "'
+               i=i+8
+           elif(t[i:i+13]=='", "title": "'):
+               output=output+'", "title": "'
+               i=i+13
+           elif(t[i:i+11]=='", "dop": "'):
+               output=output+'", "dop": "'
+               i=i+11
+           elif(t[i:i+12]=='", "text": "'):
+               output=output+'", "text": "'
+               i=i+12
+           elif(t[i:i+3]=='" }'):
+               output=output+'" }'
+               i=i+3
+           else:
+               i=i+1
+       elif(t[i]=='\\'):
+           i=i+1
+       else:
+           output=output+t[i]
+           i=i+1
+   return output
+
+fh=open("2014news1000.json","r")
+lines=fh.readlines()
+fh.close()
+for i in range(len(lines)):
+	lines[i]=clean(lines[i])
+fh=open("cleaned.json","w")
+fh.writelines(lines)
